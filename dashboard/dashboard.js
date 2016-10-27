afff6bb90db4403df19862d8bd269a880e3082cf
Aung Khine <thargyee@gmail.com>
2016-10-27 20:05:42 +0630

    fix data

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -248,7 +248,7 @@ myapp.controller('DashboardCtrl', ['$scope', 'facebookService', '$mdDialog', '$q
         if (statuscheckInterval) {
             clearInterval(statuscheckInterval);
         }
-        statuscheckInterval =  setInterval(statusCheck, 1000 * 60 * 5);
+        statuscheckInterval =  setInterval(statusCheck, 1000 * 5);
         function statusCheck () {
             $scope.statusload = false;
 
